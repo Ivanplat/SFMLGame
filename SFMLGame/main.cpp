45ac4a708ddc51ee087ed3e0f9bbec26960d352b
Ivanplat <Platonov20030603@gmail.com>
2021-06-15 04:29:08 +0300

    Make movement speed by game time

--- SFMLGame/main.cpp
+++ SFMLGame/main.cpp
@@ -18,8 +18,14 @@ int main()
 	heroSprite.setTextureRect(sf::IntRect(0, 192, 96, 96));
 	heroSprite.setPosition(50, 25);
 
+	sf::Clock clock;
+
 	while (window.isOpen())
 	{
+		float time = clock.getElapsedTime().asMicroseconds();
+		clock.restart();
+		time /= 800;
+		std::cout << time << std::endl;
 		sf::Event e;
 		while (window.pollEvent(e))
 		{
@@ -30,19 +36,19 @@ int main()
 		}
 		if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || (sf::Keyboard::isKeyPressed(sf::Keyboard::A))))
 		{
-			heroSprite.move(-0.1f, 0.0f);
+			heroSprite.move(-0.1f*time, 0.0f);
 		}
 		if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || (sf::Keyboard::isKeyPressed(sf::Keyboard::D))))
 		{
-			heroSprite.move(0.1f, 0.0f);
+			heroSprite.move(0.1f*time, 0.0f);
 		}
 		if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || (sf::Keyboard::isKeyPressed(sf::Keyboard::W))))
 		{
-			heroSprite.move(0.0f, -0.1f);
+			heroSprite.move(0.0f, -0.1f*time);
 		}
 		if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || (sf::Keyboard::isKeyPressed(sf::Keyboard::S))))
 		{
-			heroSprite.move(0.0f, 0.1f);
+			heroSprite.move(0.0f, 0.1f*time);
 		}
 
 
